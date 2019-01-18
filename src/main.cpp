4c435c8fa97b3c8b07ed46990e291735685e4773
Nicolas Misk <nicolas@debian.home>
2019-01-18 13:22:10 +0100

    algorithm: Add first version of the simulated annealing, and a call example in the main file
    Currently, the temperature is constant, and we use the exponential decision law.
    Optional parameters permit to reduce the maximum number of iterations or the minimum
    energy accepted as an optimum.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 # include <fstream>
+# include <iostream>
 
-# include "person.hpp"
+# include "simulated-annealing.hpp"
+# include "criterions.hpp"
+
+std::ostream& operator <<(std::ostream& o, const mr_fields::Person& p)
+{
+    o << p.sex << ' ' << p.cleanliness << ' ' << p.food << ' ' << p.hobby;
+    return o;
+}
+
+mr_fields::rand_init ri_;
 
 int main()
 {
-    std::ifstream in("./tests/60a.txt");
-    mr_fields::parse_from_file(in);
+    std::ifstream in("./tests/600a.txt");
+    auto persons = mr_fields::parse_from_file(in);
+    mr_fields::state s = mr_fields::simulated_annealing(persons, 1000000, 40);
+    std::cout << "Final energy: " << mr_fields::compute_energy(s, persons, mr_fields::parity_criterion) << std::endl;
     return 0;
 }
---
+++ src/simulated-annealing.hpp
@@ -0,0 +1,73 @@
+#ifndef MARKOV_RANDOM_FIELDS_SIMULATED_ANNEALING_HPP
+#define MARKOV_RANDOM_FIELDS_SIMULATED_ANNEALING_HPP
+
+# include <vector>
+# include <cstdlib>
+# include <ctime>
+# include <math.h>
+# include <limits>
+
+# include "person.hpp"
+# include "state.hpp"
+# include "criterions.hpp"
+
+namespace mr_fields
+{
+    template<typename first_criterion, typename... criterions>
+    unsigned compute_energy(const state& s, const std::vector<Person>& persons, first_criterion c, criterions... cts)
+    {
+        return c(s, persons) + compute_energy(s, persons, cts...);
+    }
+    unsigned compute_energy(const state&, const std::vector<Person>&) { return 0; }
+
+    double constant_temperature(unsigned)
+    {
+        return 1;
+    }
+
+    double exponential_law(unsigned delta_e, double temperature)
+    {
+        return exp(-(double)delta_e / temperature);
+    }
+
+    using temperature_schedule_t = decltype(constant_temperature);
+    using acceptance_law_t = decltype(exponential_law);
+
+    template<typename temperature_schedule=temperature_schedule_t,
+             typename acceptance_law=acceptance_law_t>
+    state simulated_annealing(const std::vector<Person>& persons,
+                              size_t k_max=std::numeric_limits<size_t>::max(), unsigned energy_min=0,
+                              temperature_schedule temp=constant_temperature,
+                              acceptance_law P=exponential_law)
+    {
+        state s = random_state(persons);
+        unsigned energy = compute_energy(s, persons, parity_criterion);
+        state opt = s;
+        unsigned opt_energy = energy;
+
+        for (size_t k = 0; k < k_max && energy > energy_min; k++)
+        {
+            state new_s = s.compute_neighbor();
+            unsigned new_energy = compute_energy(new_s, persons, parity_criterion);
+            if (new_energy < energy || ((double)rand()) / RAND_MAX < P(new_energy - energy, temp(k / k_max)))
+            {
+                s = new_s;
+                energy = new_energy;
+            }
+            if (energy < opt_energy)
+            {
+                opt = s;
+                opt_energy = energy;
+                std::cout << opt_energy << std::endl;
+            }
+        }
+        return std::move(opt);
+    }
+
+    struct rand_init {
+        rand_init() { std::srand(std::time(0)); }
+    };
+    extern rand_init ri_;
+}
+
+#endif //MARKOV_RANDOM_FIELDS_SIMULATED_ANNEALING_HPP
